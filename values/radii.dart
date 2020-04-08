cc16d4a77e70718508ef97d4e5d9c5cf233a8928
Joshua Hobbs <jdhobbs1990@gmail.com>
2020-04-08 16:25:19 -0500

    Starting to move important files for UI from old code, currently adding the styles and values folders

---
+++ values/radii.dart
@@ -0,0 +1,14 @@
+/*
+*  radii.dart
+*  ScoutBoard
+*
+*  Created by [Author].
+*  Copyright © 2018 [Company]. All rights reserved.
+    */
+
+import 'package:flutter/rendering.dart';
+
+
+class Radii {
+  static const BorderRadiusGeometry k23pxRadius = BorderRadius.all(Radius.circular(23));
+}
