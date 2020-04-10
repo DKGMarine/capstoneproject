bbec4dcc93d68620814800bbb93b65f4865d0580
BassamMetwally <BassamMetwally@users.noreply.github.com>
2020-04-10 12:52:44 -0500

    Added multiple routes and database functions

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,6 +5,9 @@ from overview import overview
 from teams import teams
 from fundraiserGroups import fundraiserGroups
 from addFundraiserGroup import addFundraiserGroup
+from teamsWithFundrasier import teamsWithFundraiser
+from teamMembers import teamMembers
+from teamsWithNoFundraiser import teamsWithNoFundraiser
 
 app = Flask(__name__)
 
@@ -14,7 +17,12 @@ app.register_blueprint(overview)
 app.register_blueprint(teams)
 app.register_blueprint(fundraiserGroups)
 app.register_blueprint(addFundraiserGroup)
-
+#app.register_blueprint(addTeams)
+#app.register_blueprint(forgotPassword)
+#app.register_blueprint(changePassword)
+app.register_blueprint(teamMembers)
+app.register_blueprint(teamsWithNoFundraiser)
+app.register_blueprint(teamsWithFundraiser)
 
 if __name__ == '__main__':
     app.run(debug=True)
