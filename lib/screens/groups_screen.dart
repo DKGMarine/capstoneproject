0b6fce5e333b57bfe52e0d1e25d794212a0a1ece
karlarodriguez <karlarodriguez@my.unt.edu>
2020-04-30 19:06:45 -0500

    scouts page is able to retrieve from data and add now

--- lib/screens/groups_screen.dart
+++ lib/screens/groups_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/foundation.dart';
+import '../color/colors.dart';
 import '../screens/dashboard_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:scoutboard/screens/groups_detail_screen.dart';
 import 'package:scoutboard/widgets/globals.dart' as global;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'groups_detail_screen.dart';
 
 /*
 RETRIEVING MEMBERS:
@@ -27,100 +30,6 @@ Page: '/addScoutMembers'
 
 */
 
-/*class Album {
-  // change this to parse what data you need
-
-  final String firstName;
-  final String lastName;
-  final String sold;
-  final String stock;
-  final String rank;
-  final String scoutID;
-
-  Album(
-      {this.firstName,
-      this.lastName,
-      this.sold,
-      this.stock,
-      this.rank,
-      this.scoutID});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      firstName: json['FirstName'],
-      lastName: json['LastName'],
-      sold: json['Sold'],
-      stock: json['Stock'],
-      rank: json['Rank'],
-      scoutID: json['ScoutID'],
-    );
-  }
-}
-
-List<Album> parseAlbums(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-
-  return parsed.map<Album>((json) => Album.fromJson(json)).toList();
-}
-
-Future<List<Album>> createAlbum(String id) async {
- //ID = '25275';
-  final http.Response response = await http.post(
-      'http://scoutboard.appspot.com/members', // change this to what page you are requesting data from
-
-      body: {
-        'ScoutID': id,
-      });
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 CREATED response,
-    // then parse the JSON.
-
-    print('1');
-    print(json.decode(response.body));
-    try {
-      return compute(parseAlbums, response.body);
-    } catch (err) {
-      print("Error in returning album");
-      return null;
-    }
-  } else {
-    // If the server did not return a 201 CREATED response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-Future addAlbum(String firstName, String lastName, String sold,
-    String stock, String rank, String id) async {
-  //ID = '25275';
-  final http.Response response = await http.post(
-      'http://scoutboard.appspot.com/addScoutMembers', // change this to what page you are requesting data from
-
-      body: {
-        'FirstName': firstName,
-        'LastName': lastName,
-        'Stock': stock,
-        'Sold': sold,
-        'Stock': stock,
-        'ScoutID': id,
-      });
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 CREATED response,
-    // then parse the JSON.
-
-    print('1');
-    print(json.decode(response.body));
-    return response.body;
-  } else {
-    // If the server did not return a 201 CREATED response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-*/
-
 /*
 -------------------
 
@@ -137,6 +46,7 @@ Future addAlbum(String firstName, String lastName, String sold,
     
   );
 }*/
+/*
 Future createAlbum(String firstName, String lastName, String sold,
     String stock, String rank, String scoutID) async {
 
@@ -319,3 +229,392 @@ class GroupsScreenState extends State<GroupsScreen>{
 
 
 }
+
+*/
+
+
+
+class Album {
+  // change this to parse what data you need
+
+  final String firstName;
+  final String lastName;
+  final String sold;
+  final String stock;
+  final String rank;
+  final String scoutID;
+
+  Album(
+      {this.firstName,
+      this.lastName,
+      this.sold,
+      this.stock,
+      this.rank,
+      this.scoutID});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      firstName: json['FirstName'],
+      lastName: json['LastName'],
+      sold: json['Sold'],
+      stock: json['Stock'],
+      rank: json['Rank'],
+      scoutID: json['ScoutID'],
+    );
+  }
+}
+
+List<Album> parseAlbums(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Album>((json) => Album.fromJson(json)).toList();
+}
+
+Future<List<Album>> createAlbum(String id) async {
+  final http.Response response = await http.post(
+      'http://scoutboard.appspot.com/members', // change this to what page you are requesting data from
+
+      body: {
+        'ScoutID': id,
+      });
+
+ if (response.statusCode == 200) {
+    // If the server did return a 200 CREATED response,
+    // then parse the JSON.
+
+    print('1');
+    print(response.body);
+    //print(json.decode(response.body));
+    try {
+      return compute(parseAlbums, response.body);
+    } catch (err) {
+      print("Error in returning album");
+      return null;
+    }
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+Future addAlbum(String firstName, String lastName, String sold, String stock,
+    String rank, String id) async {
+  //ID = '25275';
+  final http.Response response = await http.post(
+      'http://scoutboard.appspot.com/addScoutMembers', // change this to what page you are requesting data from
+
+      body: {
+        'FirstName': firstName,
+        'LastName': lastName,
+        'Sold': sold,
+        'Stock': stock,
+        'Rank': rank,
+        'ScoutID': id,
+      });
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 CREATED response,
+    // then parse the JSON.
+
+    print('1');
+    print(response.body);
+   // print(json.decode(response.body));
+    return response.body;
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+
+class GroupsScreen extends StatefulWidget {
+  static const routeName = '/groups-screen';
+  GroupsScreen({Key key}) : super(key: key);
+
+  @override
+  GroupsScreenState createState() => GroupsScreenState();
+}
+
+class GroupsScreenState extends State<GroupsScreen> {
+  void _startAddNewMember(BuildContext ctx) {
+    showModalBottomSheet(
+      backgroundColor: Color.fromRGBO(255, 0, 0, 0.0),
+      isScrollControlled: true,
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewScout(),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pushNamed(StatefulWidgetReg.routeName);
+            }),
+        elevation: 0,
+        title: Text(
+          'Members',
+        ),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.add), 
+            onPressed: () {
+            setState(() {
+              _startAddNewMember(context);
+              
+              
+            });
+            
+            })
+        ],
+      ),
+      body: FutureBuilder<List<Album>>(
+          future: createAlbum(global.userID),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+
+            return snapshot.hasData
+                ? SingleChildScrollView(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          ScoutsList(
+                            futureAlbum: snapshot.data,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Center(child: CircularProgressIndicator());
+          }),
+        
+    );
+  }
+}
+
+class ScoutsList extends StatefulWidget {
+  static const routeName = '/groups-screen';
+  final List<Album> futureAlbum;
+  
+
+  ScoutsList({Key key, this.futureAlbum}) : super(key: key);
+
+  @override
+  _ScoutsListState createState() => _ScoutsListState();
+}
+
+class _ScoutsListState extends State<ScoutsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 700,
+      child: widget.futureAlbum.isEmpty
+          ? Container(
+              alignment: Alignment(0.0, -0.3),
+              child: Text(
+                'No Scouts added yet!',
+                style: Theme.of(context).textTheme.title,
+                textAlign: TextAlign.center,
+              ))
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  color: Colors.blueGrey,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  //child: ListView(
+                    //actionPane: SlidableDrawerActionPane(),
+                   // actionExtentRatio: 0.25,
+                     child:ListTile(
+                  
+                      title: Text(widget.futureAlbum[index].firstName,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: AppColors.primaryText,
+                          )),
+                    
+                      subtitle: Text(widget.futureAlbum[index].lastName,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: AppColors.primaryText,
+                          )),
+                          onTap: (){
+                            var route = new MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                              new GroupdetailWidget()
+                              );
+                              Navigator.of(context).push(route);
+                          },
+                     ),
+
+                );
+              },
+              itemCount: widget.futureAlbum.length,
+            ),
+    );
+  }
+}
+
+class NewScout extends StatefulWidget {
+  @override
+  _NewScoutState createState() => _NewScoutState();
+}
+
+class _NewScoutState extends State<NewScout> {
+  List<String> names = [];
+  String enteredfirstName;
+  String enteredlastName;
+  String enteredsold;
+  String enteredstock;
+  String enteredrank;
+  String temp;
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _soldController = TextEditingController();
+  final _stockController = TextEditingController();
+  final _rankController = TextEditingController();
+
+  void _submitData() {
+    if (_firstNameController.text.isEmpty) {
+      return;
+    }
+    final enteredfirstName = _firstNameController.text;
+    final enteredlastName = _lastNameController.text;
+    final enteredsold = _soldController.text;
+    final enteredstock = _stockController.text;
+    final enteredrank = _rankController.text;
+
+    if (enteredfirstName.isEmpty ||
+        enteredlastName.isEmpty
+      ) {
+      return;
+    }
+
+    print(enteredfirstName);
+    print(enteredlastName);
+    String response;
+    addAlbum(enteredfirstName, enteredlastName, enteredsold, enteredstock,enteredrank, global.userID)
+        .then((response) {
+      print('88');
+      print(response);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Card(
+        margin: EdgeInsets.all(10),
+        elevation: 5,
+        child: Container(
+          color: Color.fromRGBO(40, 73, 100, 1),
+          padding: EdgeInsets.all(10),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                  ),
+                  decoration: InputDecoration(
+                    labelText: 'First Name',
+                    labelStyle: TextStyle(
+                      color: AppColors.primaryText,
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.primaryText,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.primaryText,
+                      ),
+                    ),
+                  ),
+                  controller: _firstNameController,
+                  onChanged: (text){
+                    temp = text;
+                  },
+                ),
+                TextField(
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                  ),
+                  decoration: InputDecoration(
+                    labelText: 'Last Name',
+                    labelStyle: TextStyle(
+                      color: AppColors.primaryText,
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.primaryText,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: AppColors.primaryText,
+                      ),
+                    ),
+                  ),
+                  controller: _lastNameController,
+                 
+                ),
+                
+                SizedBox(height: 80),
+                ButtonTheme(
+                  minWidth: 150.0,
+                  height: 50.0,
+                  child: RaisedButton(
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(40.0),
+                    ),
+                    child: Text('Add Scouts'),
+                    color: AppColors.secondaryElement,
+                    textColor: Theme.of(context).textTheme.button.color,
+                    onPressed: () {
+                      enteredfirstName = _firstNameController.text;
+                      enteredlastName = _lastNameController.text;
+                      _soldController.text = "1";
+                      _stockController.text = "empty";
+                      _rankController.text = "empty";
+                      setState(() {
+                        names.add(temp);
+                       _submitData();
+                        //
+                      });
+                    
+                    }),
+                ),
+                SizedBox(height: 140),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
